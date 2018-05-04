3ee18d39aa5ce760b22811e168f2643358c98070
DmVillanueva <34350310+DmVillanueva@users.noreply.github.com>
2018-05-05 01:14:56 +0200

    corregido error en la linea 43

--- install_ovswitch.sh
+++ install_ovswitch.sh
@@ -40,7 +40,7 @@ sudo touch /usr/local/etc/ovs-vswitchd.conf
 sudo mkdir -p /usr/local/etc/openvswitch
 echo cargando la configuración
 sudo ovsdb-tool create /usr/local/etc/openvswitch/conf.db
-ovsdb-server --remote=punix:/usr/local/var/run/openvswitch/db.sock --remote=db:Open_vSwitch,manager_options --private-key=db:SSL,privavte_key --certificate=db:SSL,certificate --bootstrap-ca-cert=db:SSL, ca_cert --pidfile --detach
+ovsdb-server --remote=punix:/usr/local/var/run/openvswitch/db.sock --remote=db:Open_vSwitch,manager_options --private-key=db:SSL,private_key --certificate=db:SSL,certificate --bootstrap-ca-cert=db:SSL,ca_cert --pidfile --detach
 echo inicializando la base de datos
 sudo ovs-vscil --no-wait init 
 sudo ovs-vswitchd --pidfile --detach
